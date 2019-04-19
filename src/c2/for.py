05707f48596fcf44dff4c7ad929eb508f5af30ca
Go nakagawa <g.nakagawa@gemcook.com>
2019-04-19 20:14:30 +0900

    for finished

---
+++ src/c2/for.py
@@ -0,0 +1,10 @@
+# coding: UTF-8
+# practice for
+# for i in range(5):
+#     print(i)
+
+v = 0
+for i in range(1, 1311):
+    v = v + i
+    print(i, "sum =", v)
+print("sum all 1 to 10...", v)
---
+++ src/c2/while-simple.py
@@ -0,0 +1,6 @@
+# coding: UTF-8
+energy = 3
+while energy > 0:
+    print('+ Run')
+    print("| energy = ", energy)
+    energy -= 1
