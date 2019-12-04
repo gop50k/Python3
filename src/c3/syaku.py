52e485b26e5a45c1d029d06476d55ad3d51a1470
Go nakagawa <g.nakagawa@gemcook.com>
2019-12-04 22:12:43 +0900

    module done

---
+++ src/c3/syaku.py
@@ -0,0 +1,5 @@
+# coding: UTF-8
+# 尺からcmに変換するモジュール
+
+def syaku_to_cm(syaku):
+    return round(syaku * 30.303, 3)
---
+++ src/c3/test-syaku.py
@@ -0,0 +1,16 @@
+# coding: UTF-8
+# syaku.pyをモジュールとして使用するプログラム
+
+import syaku
+
+v = syaku.syaku_to_cm(10)
+print("10尺=", v, "cm")
+
+v = syaku.syaku_to_cm(20)
+print("20尺=", v, "cm")
+
+v = syaku.syaku_to_cm(100)
+print("100尺=", v, "cm")
+
+v = syaku.syaku_to_cm(0.1)
+print("0.1尺=", v, "cm")
