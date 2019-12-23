8b022ab880eabbcb13085c1741961e2d72ae8469
Go nakagawa <g.nakagawa@gemcook.com>
2019-12-23 21:01:46 +0900

    text counter done

---
+++ src/c5/text-counter.py
@@ -0,0 +1,19 @@
+# coding: UTF-8
+# 文字数をカウントするプログラム
+from tkinter import *
+
+def count_text(event):
+    s = main_text.get(1.0, END)
+    info_label.config(text='{0}文字'.format(len(s)))
+
+root = Tk()
+root.title('テキストカウンタ')
+
+main_text = Text(root)
+main_text.bind('<Key>', count_text)
+main_text.pack()
+
+info_label = Label(root)
+info_label.pack()
+
+root.mainloop()
