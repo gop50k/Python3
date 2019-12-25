7d057584da5451a245e70b91e67efa29d2603689
Go nakagawa <g.nakagawa@gemcook.com>
2019-12-25 21:49:51 +0900

    localhost done

---
+++ src/c5/cgi-bin/kakugen.py
@@ -0,0 +1,11 @@
+# coding: UTF-8
+# localhostに文字列表示するプログラム
+#!/usr/bin/env python3
+
+print("Content-Type: text/html; charset=utf-8")
+
+print("")
+
+print("<html><head><meta charset='utf-8'></head><body>")
+print("聞くことに早く語るに遅くあるべき")
+print("</body></html>")
