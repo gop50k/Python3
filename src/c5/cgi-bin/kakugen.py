18f3eb53ba1905f47a2749741ec63d322bcc1549
Go nakagawa <g.nakagawa@gemcook.com>
2019-12-27 00:07:53 +0900

    shebang modify

--- src/c5/cgi-bin/kakugen.py
+++ src/c5/cgi-bin/kakugen.py
@@ -1,11 +1,10 @@
 # coding: UTF-8
 # localhostに文字列表示するプログラム
-#!/usr/bin/env python3
-
+#! /usr/bin/env python3
 print("Content-Type: text/html; charset=utf-8")
 
 print("")
-
 print("<html><head><meta charset='utf-8'></head><body>")
 print("聞くことに早く語るに遅くあるべき")
+print("立てば芍薬座れば牡丹")
 print("</body></html>")
