2237a3c1028839bec263ee11b1c1dcacdfa138d5
Go nakagawa <g.nakagawa@gemcook.com>
2020-01-18 00:25:53 +0900

    accessor update

--- src/c5/accesor.py
+++ src/c5/accesor.py
@@ -4,10 +4,22 @@
 class Clock:
     def __init__(self, hour):
         self._hour = hour
+        self._ampm = 'am'
 
     @property
     def hour(self):
         return self._hour
 
-c = Clock(10)
-print(c.hour)
+    @property
+    def ampm(self):
+        return self._ampm
+
+    @hour.setter
+    def hour(self, value):
+        self._hour = value % 12
+        self._ampm = 'am' if value <= 12 else 'pm'
+
+obj = Clock(10)
+print(obj.hour, obj.ampm)
+obj.hour = 14
+print(obj.hour, obj.ampm)
