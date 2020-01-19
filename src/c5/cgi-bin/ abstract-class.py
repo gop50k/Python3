7577d729a727839d04a4f58a85c9895bd5622fc4
Go nakagawa <g.nakagawa@gemcook.com>
2020-01-19 12:30:44 +0900

    abstract base class done

---
+++ src/c5/cgi-bin/ abstract-class.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+# 抽象基底を実装したプログラム
+
+from abc import ABCMeta, abstractclassmethod
+
+class MazaRobot(metaclass=ABCMeta):
+
+    @abstractclassmethod
+    def __init__(self):
+        pass
+
+    @abstractclassmethod
+    def choose_dir(self):
+        pass
