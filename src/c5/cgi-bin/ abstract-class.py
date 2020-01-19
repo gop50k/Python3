9be3374185e45463f3e5b3c52522e4b8467ab65d
Go nakagawa <g.nakagawa@gemcook.com>
2020-01-19 12:38:33 +0900

    abstract base class done

--- src/c5/cgi-bin/ abstract-class.py
+++ src/c5/cgi-bin/ abstract-class.py
@@ -4,7 +4,6 @@
 from abc import ABCMeta, abstractclassmethod
 
 class MazaRobot(metaclass=ABCMeta):
-
     @abstractclassmethod
     def __init__(self):
         pass
---
+++ src/c5/cgi-bin/abstract-class-test.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+# 抽象基底をテストするプログラム
+
+from abstract-class import MazeRobot
+class MazaRobotTest(MazeRobot):
+
+    def __init__(self):
+        print('ロボットを初期化します。')
+
+    def choose_dir(self):
+        print('前進します。')
+
+robot = MazaRobotTest()
+robot.init_robot()
